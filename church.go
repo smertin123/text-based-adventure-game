fac4e2fcde158ccbf0074b2c035be0d7fac27453
smertin <smertin@gmail.com>
2022-12-19 16:32:50 +0000

    dialog

--- church.go
+++ church.go
@@ -6,7 +6,7 @@ func Church() {
 	fmt.Printf("\nYou see 2 people at the alter getting married.\n\n1. Talk to them\n2. Leave\n\n")
 	fmt.Scanln(&choice)
 	if choice == 1 {
-		fmt.Printf("\nGroom: \"The road to the east is very dangerous. I wouldnt recommend heading there without a weapon!\"\nWife: \"A woman lives in town that may help you find one.\"\n\n")
+		fmt.Printf("\nGroom: \"The road to the east is very dangerous. I wouldnt recommend heading there without a weapon!\"\nWife: \"A woman lives in town that may help you find one.\"\nGroom: \"If she cant, I would suggest checking the castle.\"\n\n")
 		Church()
 	} else if choice == 2 {
 		Scene(town)
